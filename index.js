41035aafc97a5cbf909113b34ae45b615a7baf94
Joey585 <thelich06@protonmail.com>
2025-03-13 20:09:50 -0400

    error checking

--- index.js
+++ index.js
@@ -17,13 +17,17 @@ const files = fs.readdirSync(__dirname + config.commandsFolder, {recursive: true
 files.forEach(file => {
     if(file.endsWith(".js")) {
         const commandPath = path.join(__dirname + config.commandsFolder, file);
-        const command = require(commandPath);
-        commands.set(command.name, command);
-        console.log("Loaded " + command.name);
+        try {
+            const command = require(commandPath);
+            commands.set(command.name, command);
+            console.log("Loaded " + command.name);
+        } catch (e) {
+            console.error("Error loading your command! " + e);
+        }
     }
 });
 
-client.on("messageCreate", (message) => {
+client.on("messageCreate", async (message) => {
     if(!message.content.startsWith(config.prefix)) return;
     if(message.author.bot) return;
     const commandName = message.content.trim().split(" ")[0].slice(config.prefix.length);
@@ -32,10 +36,10 @@ client.on("messageCreate", (message) => {
     const args = message.content.trim().split(" ").slice(1);
     const command = commands.get(commandName)
     if(command.permission && !(message.member.permissions.has(BigInt(command.permission)))) {
-        message.reply({content: "You don't have permission to use this command!"});
+        await message.reply({content: "You don't have permission to use this command!"});
         return;
     }
-    command.execute(message, args);
+    await command.execute(message, args);
 })
 
 
